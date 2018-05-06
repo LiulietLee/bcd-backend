257fe5678ea6ef877730ae911dbc30cdaf308146
LiulietLee <lee.liuliet@hotmail.com>
2018-05-06 16:06:05 +0800

    update

---
+++ src/Entity/SearchRecord.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SearchRecordRepository")
+ */
+class SearchRecord
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nid;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $time;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $coverURL;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?int
+    {
+        return $this->type;
+    }
+
+    public function setType(int $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getNid(): ?int
+    {
+        return $this->nid;
+    }
+
+    public function setNid(int $nid): self
+    {
+        $this->nid = $nid;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): self
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getCoverURL(): ?string
+    {
+        return $this->coverURL;
+    }
+
+    public function setCoverURL(?string $coverURL): self
+    {
+        $this->coverURL = $coverURL;
+
+        return $this;
+    }
+}
