df2921ff44918f754958da140f922973c101f574
LiulietLee <lee.liuliet@hotmail.com>
2018-05-29 18:14:52 +0800

    update spider url

--- src/Model/SpiderURLGenerator.php
+++ src/Model/SpiderURLGenerator.php
@@ -4,20 +4,29 @@ namespace App\Model;
 
 class SpiderURLGenerator {
 
-    private static $baseURL = "https://bilibilicd.herokuapp.com";
+    private static $base = "https://bilibilicd";
+
+    private static function baseURL(): string {
+        $minute = date("i");
+        if ($minute < 30) {
+            return self::$base. ".vapor.cloud";
+        } else {
+            return self::$base. ".herokuapp.com";
+        }
+    }
 
     static function avCoverInfoURLByAID(int $aid): string {
-        $fullURL = self::$baseURL. "/av/info/$aid";
+        $fullURL = self::baseURL(). "/av/info/$aid";
         return $fullURL;
     }
 
     static function cvCoverInfoURLByCID(int $cid): string {
-        $fullURL = self::$baseURL. "/cv/info/$cid";
+        $fullURL = self::baseURL(). "/cv/info/$cid";
         return $fullURL;
     }
 
     static function lvCoverInfoURLByLID(int $lid): string {
-        $fullURL = self::$baseURL. "/lv/info/$lid";
+        $fullURL = self::baseURL(). "/lv/info/$lid";
         return $fullURL;
     }
 
