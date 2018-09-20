431aed78e8a78f2ab6d7a227d5262b76ae152c39
LiulietLee <lee.liuliet@hotmail.com>
2018-09-20 13:48:16 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git pull
 sudo php bin/console cache:clear
-sudo chown -R www-data:www-data var/cache/prod/twig
+sudo chown -R www-data:www-data /var/cache/prod/twig
+sudo chown -R www-data:www-data /var/www/bcd-backend/var/cache/prod
